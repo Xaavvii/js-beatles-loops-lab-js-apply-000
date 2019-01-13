5c9ee05e446b6beecb13420986be0943bb8f421d
authentic-actor-8801 <xaviernational@aol.com>
2019-01-13 00:07:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,14 +16,6 @@ function theBeatlesPlay(musicians, instruments)
 var newArray = theBeatlesPlay(musicians, instruments);
 console.log(newArray);
 
-const facts =
-[
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
 function johnLennonFacts()
 {
   var emptyArray = [];
